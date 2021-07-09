cda4fc967f80b1e45a64709b1f31d3fdc2f48a2f
Brian L. Gorman <blgorman@gmail.com>
2021-07-09 00:09:37 -0500

    make sure migrations are automatically pushed on deployment in the startup

--- SimpleMVCWebWithAuth/Startup.cs
+++ SimpleMVCWebWithAuth/Startup.cs
@@ -39,7 +39,7 @@ namespace SimpleMVCWebWithAuth
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -51,6 +51,8 @@ namespace SimpleMVCWebWithAuth
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                //ensure migrations
+                context.Database.Migrate();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
